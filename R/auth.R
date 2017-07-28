342ceda2a65ede393c0c468ec0e7863c2b4501a8
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-28 19:53:37 +0200

    minor stops

--- R/auth.R
+++ R/auth.R
@@ -4,18 +4,18 @@
 #'
 #' @param user character. Github user name.
 #' @param password character. Github password.
-#' @return integer. Status code of the authentication response from the Github 
-#' API; \code{200} if authentication succesfull. 
+#' @return integer. Status code of the response: \code{200} if
+#' authentication succesfull.
 #'
 #' @export
 authenticate <- function(user, password) {
-  stopifnot(is.character(user), nchar(user) > 0L, 
-            is.character(password), nchar(password) > 0L)
+  stopifnot(is.character(user), length(user) == 1L,
+            is.character(password), length(password) == 1L)
   h <- curl::new_handle()
   curl::handle_setopt(h, HEADER=1L)
   curl::handle_setopt(h, NOBODY=1L)
   curl::handle_setopt(h, HTTPAUTH=1L)
   curl::handle_setopt(h, USERPWD=paste0(user, ':', password))
-  res <- curl::curl_fetch_memory('https://api.github.com/user', h)
-  return(res$status_code)
+  re <- curl::curl_fetch_memory('https://api.github.com/user', h)
+  return(re$status_code)
 }
